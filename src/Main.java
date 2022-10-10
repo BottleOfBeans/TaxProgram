f0a36997c1124e383b18d0fa1394b86a758f39f0
Romir <romir.tandon@gmail.com>
2022-10-10 12:45:38 -0400

    Finished Up Single People Code, Everything has been completed for that Mr.Gentul's checker was a little bit off that got fixed so everything is ok know. Fixed up the logic and added some debugging statements to make it easier to work on and finish. :)

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,15 @@
 public class Main {
     public static void main(String[] args) {
         //Testing by creating accounts with information
-        account test = new account(0, 25900, false, 0);
-        account test2 = new account(1,25900, true, 2);
+        account test = new account(0, 276529, false, 0, 28000);
+        account test2 = new account(1,212576, true, 2, 15728);
 
         //Testing getting the information for each account
         test.getInfo();
         test2.getInfo();
 
         //Calcualting taxes for each acccount
-        System.out.println(test.taxCalculator());
+        System.out.println(test2.taxCalculator());
 
 
     }
--- src/account.java
+++ src/account.java
@@ -9,20 +9,24 @@ public class account {
     public double income;
     public boolean isMarried;
     public int children;
+    public double deductions;
 
     public double[] taxPercents = {0.1,0.12,0.22,0.24,0.32,0.35,0.37};
-    public double[] maxRates = {10275,41775,89075,170050,215950,539900,539901};
+    public double[] maxRates = {10275,41775,89075,170050,215950,539900,99999999};
+    public double[] minRates = {0,10275,41775,89075,170050,215950,539901};
     public double[] marriedMaxRates ={20550,83550,178150,340100,431900,647850,647850};
+    public double[] marriedMinRates ={0,20551,83551,178151,340101,431901,647850};
     /*
         Constructors:
         Take in the account ID and balance, and put store them
         If no balance is given then it is put to 0
      */
-    public account(int gID, double gBalance, boolean gIsMarried, int gChildren){
+    public account(int gID, double gBalance, boolean gIsMarried, int gChildren, int gdeductions){
         ID = gID;
         income = gBalance;
         isMarried = gIsMarried;
         children = gChildren;
+        deductions = gdeductions;
     }
     public account(int gID){
         ID = gID;
@@ -51,15 +55,11 @@ public class account {
         income may fall into
      */
     public int maxBracket(){
+        double tempIncome = income-deductions;
         int maxBracket =0;
-        for(int i=0; i<maxRates.length; i++){
-            if(income > maxRates[maxRates.length-1]){
-                maxBracket = maxRates.length-1;
-                break;
-            }
-            else if(maxRates[i]<income && maxRates[i+1] > income){
+        for(int i=0; i<maxRates.length; i++) {
+            if (tempIncome > minRates[i] && tempIncome < maxRates[i]) {
                 maxBracket = i;
-                break;
             }
         }
         return maxBracket;
@@ -77,22 +77,32 @@ public class account {
     public String taxCalculator() {
         double incomeTax = 0;
         double childBenefits = children*2000;
-        double tempIncome = income;
+        double tempIncome = income-deductions;
 
         if(isMarried){
             maxRates = marriedMaxRates;
+            minRates = marriedMinRates;
         }
+        System.out.println("Max Percent: " + taxPercents[maxBracket()]);
 
-        for (int x = maxBracket()-1; x > 1; x--) {
-                incomeTax += (tempIncome-maxRates[x-1])*taxPercents[x];
-                tempIncome -= (tempIncome-maxRates[x-1]);
-            }
-        incomeTax += tempIncome*taxPercents[0];
+        for(int x = maxBracket(); x>=0; x--){
+            /*/
+            Debugging print statements, give outs information that was used in the debugging process (irrelevant to the final output)
+             */
+            System.out.print("Round "+x+": "+ tempIncome + " - " + minRates[x] + " = " +(tempIncome-minRates[x]));
+            System.out.print("    @"+taxPercents[x]+"% = "+((tempIncome-minRates[x])*taxPercents[x]));
+            System.out.print("\n");
+
+            double tax = (tempIncome-minRates[x])*taxPercents[x];
+            incomeTax += tax;
+            tempIncome = minRates[x];
+        }
+        
         incomeTax -= childBenefits;
         return (
                         "\n===============\n"+
                         "Total Federal Income Tax: $"+incomeTax+"\n"+
-                        "Child Tax Credit: $"+(incomeTax+childBenefits)+"\n"+
+                        "Child Tax Credit: $"+childBenefits+"\n"+
                         "Total Owed: $"+incomeTax+"\n"+
                         "Paycheck After Federal Tax: $"+(income-incomeTax)+
                         "\n===============\n"
